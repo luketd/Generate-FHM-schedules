34fa665560f02c85e632dff4d069d6707eba350c
luketd <lukedamato99@gmail.com>
2021-07-01 18:31:48 -0400

    Updated schedules
    make it so that the game order is the same in the FHM schedule

--- Schedules.R
+++ Schedules.R
@@ -17,33 +17,53 @@ schedules$year <- year(ymd(schedules$Date))
 schedules$month <- month(ymd(schedules$Date))
 schedules$day <- day(ymd(schedules$Date))
 schedules$IRLDay <- 0
+schedules$gameNumber <- 0
+gameNum <- 1
+#getting the games right
+for (x in 1:length(schedules$day)){
+  schedules$gameNumber[x] <- gameNum
+  gameNum <- gameNum +1
+}
 
 IRLDay <- 1
 ingameDay <- schedules$day[1] #First day of RS
-targetGamePerDay <- 13 #change when Simmer tells you to
+daysInSeason <- 25 #How many days in the season
+targetGamePerDay <- (length(schedules$day) / daysInSeason) - 1
 iterator <- 0
 #start with IRLDay 1, iterate the schedule until it reaches 22 games, then have it stop at the end of the day
 
 for (i in 1:length(schedules$day)){
   schedules$IRLDay[i] <- IRLDay
-  if (iterator == targetGamePerDay){
-    if (schedules$day[i] == schedules$day[i+1])  {
-      schedules$IRLDay[i] <- IRLDay
-    } else {
-      IRLDay <- IRLDay + 1
-      iterator <- 0
+  #print((iterator/targetGamePerDay)* 100)
+  if ((iterator/targetGamePerDay)* 100 > 91 ) { #check to see if the "target" day is over 91% of the way there
+    if (schedules$day[i] == schedules$day[i+1]){
+      #check to see if the next day lands on the same sim day
+      if (schedules$day[i] == schedules$day[i-1]){ 
+        #check to make sure that the sim day lands on the day before
+        schedules$IRLDay[i] <- IRLDay
+      } else {
+        #if it isnt, then increase the IRL day and make that sim day the next irl day
+        IRLDay <- IRLDay + 1
+        schedules$IRLDay[i] <- IRLDay #Make the IRL the next day
+        iterator <- 0
+      }
     }
   } else {
-    iterator <- iterator + 1
+    schedules$IRLDay[i] <- IRLDay
   }
   i <- i+1
+  iterator <- iterator +1
   
+
 }
 
 
 GPerD <- schedules %>%
   count(IRLDay)
 
+####################################################
+#This gets the team abbreviations for the teamID's
+#####################################################
 
 library(readr)
 team_data <- read_delim("team_data.csv", 
@@ -52,15 +72,22 @@ team_data <- read_delim("team_data.csv",
 team_data <- team_data %>%
   select(TeamId, Abbr)
 
-
 schedulesNew <- merge(schedules, team_data, by.x = "Home", by.y = "TeamId")
 
 schedulesNew <- merge(schedulesNew, team_data, by.x = "Away", by.y = "TeamId")
 
-schedulesNew <- schedulesNew[with(schedulesNew, order(Date)), ]
+schedulesNew <- schedulesNew[with(schedulesNew, order(Date)), ] %>%
+  arrange(gameNumber, decreasing=TRUE)
+
+####################################################################
+#After this it could be a lot better
+####################################################################
+
+
 
 IRLStartDate <- as.Date("2021/06/24")
 weekdays(as.Date(IRLStartDate))
+
 
 test <- list()
 
@@ -94,5 +121,6 @@ for (x in 1:(length(schedules$day) +1 )) {
 finalSchedule <- do.call("rbind", test)
 
 
-write.csv(finalSchedule,"C:\\Users\\Luke\\Desktop\\SHL\\SMJHLSchedule.csv", row.names = FALSE)
+write.csv(finalSchedule,"C:\\Users\\Luke\\Desktop\\SHL\\SHLSchedule.csv", row.names = FALSE)
+
 
